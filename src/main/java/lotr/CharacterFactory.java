6555e3eb019e935ad98cc34b5c3f79dbdd67ab94
Dziuba Oksana <oxanadziuba2007@gmail.com>
2025-10-04 17:41:49 +0300

    creating CharacterFactory to create random characters

---
+++ src/main/java/lotr/CharacterFactory.java
@@ -0,0 +1,23 @@
+package lotr;
+
+import java.util.Random;
+
+public class CharacterFactory {
+    public Character createCharacter() {
+        Random random = new Random();
+        int characterType = random.nextInt(4);
+
+        switch (characterType) {
+            case 0:
+                return new Hobbit();
+            case 1:
+                return new Elf();
+            case 2:
+                return new King();
+            case 3:
+                return new Knight();
+            default:
+                return new Hobbit();
+        }
+    }
+}
